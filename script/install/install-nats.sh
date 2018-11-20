5d29a6a327bcc79ddc0e9df3fff061d0a35bf91c
PureDu <dxbinlive@gmail.com>
2018-11-20 14:45:33 +0800

    nats install script

---
+++ script/install/install-nats.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+apt-get install -y zip
+mkdir /tmp/nats
+cd /tmp/nats
+
+wget https://github.com/nats-io/gnatsd/releases/download/v$1/gnatsd-v$1-linux-amd64.zip
+unzip gnatsd-v$1-linux-amd64.zip
+mkdir -p /usr/local/nats/bin
+cp gnatsd-v$1-linux-amd64/* /usr/local/nats/bin/
+rm -Rf /tmp/nats
+
+
+cat > /usr/local/nats/gnatsd.config <<END
+port: 4222
+http_port: 8222
+
+cluster {
+    port: 5222
+}
+END
+
+adduser --system --group --no-create-home --shell /bin/false nats
+chown -R nats:nats /usr/local/nats
+
+cat > /etc/systemd/system/nats.service <<end
+[Unit]
+Description=NATS messaging server
+
+[Service]
+ExecStart=/usr/local/nats/bin/gnatsd -c /usr/local/nats/gnatsd.config
+User=nats
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+end
+
+systemctl enable nats
+systemctl start nats
